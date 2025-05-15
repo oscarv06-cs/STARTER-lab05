3f2c272c0de3b37aa3150c9c7bd9513fa7afd57e
Oscar Ramon Valeriano Leiva <oscarvaleriano@csilvm-12.cs.ucsb.edu>
2025-05-14 17:41:33 -0700

    Started tests.cpp

---
+++ tests.cpp
@@ -0,0 +1,2 @@
+// These files should contain test code for all the classes and methods you used in your game with 
+// the custom BST (main.cpp). We recommend at least 5 test cases for each public member function. 
