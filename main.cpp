babdd9f704b81e1a994cd79c24355e5c1636d1a1
Oscar Ramon Valeriano Leiva <oscarvaleriano@csilvm-12.cs.ucsb.edu>
2025-05-16 22:51:09 -0700

    Fixed

--- main.cpp
+++ main.cpp
@@ -1,89 +1,107 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <sstream>
+#include <string>
 #include "card.h"
 #include "card_list.h"
+// Do not include set in this file
 
 using namespace std;
 
-int main(int argc, char** argv) {
-    if (argc < 3) {
-        cout << "Please provide 2 file names" << endl;
-        return 1;
-    }
+int main(int argc, char* argv[]) {
+  if(argc < 3){
+    cout << "Please provide 2 file names" << endl;
+    return 1;
+  }
 
-    ifstream cardFile1(argv[1]);
-    ifstream cardFile2(argv[2]);
-    if (cardFile1.fail() || cardFile2.fail()) {
-        cout << "Could not open file " << argv[2] << endl;
-        return 1;
-    }
+  ifstream cardFile1(argv[1]);
+  ifstream cardFile2(argv[2]);
+  string line;
 
-    bst alice;
-    bst bob;
-    string line;
+  if (cardFile1.fail() || cardFile2.fail()) {
+    cout << "Could not open file " << argv[2];
+    return 1;
+  }
 
-    while (getline(cardFile1, line) && !line.empty()) {
-        stringstream ss(line);
-        string suit, value;
-        ss >> suit >> value;
-        Card card(suit[0], value);
-        alice.insert(card);
-    }
-    cardFile1.close();
-
-    while (getline(cardFile2, line) && !line.empty()) {
-        stringstream ss(line);
-        string suit, value;
-        ss >> suit >> value;
-        Card card(suit[0], value);
-        bob.insert(card);
-    }
-    cardFile2.close();
-
-    bool match_found = true;
-
-while (match_found) {
-    match_found = false;
-
-    // Alice's turn
-    for (auto it = alice.begin(); it != alice.end(); ++it) {
-        if (bob.contains(*it)) {
-            Card match = *it;
-            alice.remove(match);
-            bob.remove(match);
-            cout << "Alice picked matching card " << match << endl;
-            match_found = true;
-            break;
-        }
-    }
-
-    // Bob's turn (no "continue" in between)
-    for (auto it = bob.rbegin(); it != bob.rend(); ++it) {
-        if (alice.contains(*it)) {
-            Card match = *it;
-            alice.remove(match);
-            bob.remove(match);
-            cout << "Bob picked matching card " << match << endl;
-            match_found = true;
-            break;
-        }
-    }
-}
-
+  CardList alice;
+  CardList bob;
   
-cout << endl << "Alice's cards:" << endl;
-for (auto it = alice.begin(); it != alice.end(); ++it) {
-    cout << *it << endl;
-}
+  //Read each file
+  while (getline(cardFile1, line) && (line.length() > 0)) {
+    Suit suit;
+    string value;
+
+    istringstream iss(line);
+    char temp;
+    iss >> temp >> value;
+
+    if(temp == 'c') {suit = Suit::clubs;}
+    else if (temp == 'd') {suit = Suit::diamonds;}
+    else if(temp == 's') {suit = Suit::spades;}
+    else{suit = Suit::hearts;}
+
+    alice.insert(Card(suit, value));
+  }
+
+  cardFile1.close();
 
 
-cout << endl <<"Bob's cards:" << endl;
-for (auto it = bob.rbegin(); it != bob.rend(); ++it) {
-    cout << *it << endl;
-}
+  while (getline(cardFile2, line) && (line.length() > 0)) {
+    Suit suit;
+    string value;
 
+    istringstream iss(line);
+    char temp;
+    iss >> temp >> value;
 
-    return 0;
+    if(temp == 'c') {suit = Suit::clubs;}
+    else if (temp == 'd') {suit = Suit::diamonds;}
+    else if(temp == 's') {suit = Suit::spades;}
+    else{suit = Suit::hearts;}
+
+    bob.insert(Card(suit, value));
+
+  }
+
+  cardFile2.close();
+
+  bool match = true;
+  while(match){
+    match = false;
+
+    const Card* a = alice.minValue();
+    while(a){
+      if(bob.find(*a)){
+        cout << "Alice picked matching card " << *a << endl;
+        bob.remove(*a);
+        alice.remove(*a);
+
+        match = true;
+        break;
+      }
+      a = alice.successor(*a);
+    }
+
+    const Card* b = bob.maxValue();
+    while(b){
+      if(alice.find(*b)){
+        cout << "Bob picked matching card " << *b << endl;
+        alice.remove(*b);
+        bob.remove(*b);
+
+        match = true;
+        break;
+      }
+      b = bob.predecessor(*b);
+    }
+  }
+
+  cout << "\nAlice's cards: " << endl;
+  alice.printInOrder();
+  cout << endl;    
+  
+  cout << "Bob's cards: " << endl;
+  bob.printInOrder();
+
+  return 0; 
 }
--- main_set.cpp
+++ main_set.cpp
@@ -2,87 +2,139 @@
 // Do not include card_list.h in this file
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <set>
 #include "card.h"
-#include <sstream>
 
-/*Std::Set is a container which stroes elements in a sroted order and does not allow duplicates. It automatically keeps data sorted using comparisons like <. This is important
- for the game we're coding. The operations are also fast, insert, delete, and search all have O(logn) time*/
 using namespace std;
 
-std::set<Card> alice;
-std::set<Card> bob;
-
-int main(int argc, char** argv){
-  if(argc < 3){
+int main(int argc, char* argv[]) {
+  if(argc < 3) {
     cout << "Please provide 2 file names" << endl;
     return 1;
   }
-  
-  ifstream cardFile1 (argv[1]);
-  ifstream cardFile2 (argv[2]);
+
+  ifstream cardFile1(argv[1]);
+  ifstream cardFile2(argv[2]);
   string line;
 
-  if (cardFile1.fail() || cardFile2.fail() ){
+  if (cardFile1.fail() || cardFile2.fail()) {
     cout << "Could not open file " << argv[2];
     return 1;
   }
 
+
+  set<Card> alice;
+  set<Card> bob;
+
   //Read each file
-  while (getline (cardFile1, line) && (line.length() > 0)){
-    stringstream ss(line);
-    string suit, value;
-    ss >> suit >> value;
-    Card card(suit[0], value);
-    alice.insert(card);
+  while (getline(cardFile1, line) && (line.length() > 0)) {
+    Suit suit;
+    string value;
+
+    istringstream iss(line);
+    char temp;
+    iss >> temp >> value;
+
+    if(temp == 'c') {suit = clubs;}
+    else if (temp == 'd') {suit = diamonds;}
+    else if(temp == 's') {suit = spades;}
+    else{suit = hearts;}
+
+    alice.insert(Card(suit, value));
 
   }
+
   cardFile1.close();
 
+  while (getline(cardFile2, line) && (line.length() > 0)) {
+    Suit suit;
+    string value;
 
-  while (getline (cardFile2, line) && (line.length() > 0)){
-    stringstream ss(line);
-    string suit, value;
-    ss >> suit >> value;
-    Card card(suit[0], value);
-    bob.insert(card);
+    istringstream iss(line);
+    char temp;
+    iss >> temp >> value;
 
+    if(temp == 'c') {suit = clubs;}
+    else if (temp == 'd') {suit = diamonds;}
+    else if(temp == 's') {suit = spades;}
+    else{suit = hearts;}
+
+    bob.insert(Card(suit, value));
   }
-  cardFile2.close();
-  bool match_found = true;
-  while (match_found != false){ //Check if no matches have been found
-    match_found = false;
-    //Alice
-    for (auto it = alice.begin(); it != alice.end(); ++it) // Iterator to iterate through alice's cards
-      if (bob.find(*it) != bob.end()){
-        Card match = *it;
-        alice.erase(match);
-        bob.erase(match);
-        match_found = true;
-        cout << "\nAlice picked matching card " << match << endl;
-        break; //Ends the turn
-      }
 
-    for (auto it = bob.rbegin(); it != bob.rend(); ++it) // Iterator to iterate through bob's cards
-      if (alice.find(*it) != alice.end()){
-      Card match = *it;
-      alice.erase(match);
-      bob.erase(match);
-      match_found = true;
-      cout << "\nBob picked matching card " << match << endl;
-      break; //Ends the turn
+  cardFile2.close();
+
+  bool match = true;
+  while(match){
+    match = false;
+
+    for(auto it = alice.begin(); it != alice.end();){
+
+      if(bob.find(*it) != bob.end()){
+        cout << "Alice picked matching card " << *it << endl;
+
+        bob.erase(*it);
+        it = alice.erase(it);
+
+        match = true;
+        break;
+      }
+      else{
+        it++;
+      }
+      
+    }
+
+    for(auto it = bob.rbegin(); it != bob.rend();){
+
+      if(alice.find(*it) != alice.end()){
+        cout << "Bob picked matching card " << *it << endl;
+
+        alice.erase(*it);
+        bob.erase(next(it).base());
+
+        match = true;
+        break;
+      }
+      else{
+        it++;
+      }
+      
     }
   }
-  //Iterates through Alice's set and outputs her remaining cards
-  cout << "Alice's cards" << endl;
-  for (auto it = alice.begin(); it != alice.end(); ++it){
-    cout << *it << endl;
+
+  
+  char type;
+
+  cout << "\nAlice's cards: " << endl;
+  for(auto elem : alice){
+    if(elem.getSuit() == 0){
+      type = 'c';
+    }else if(elem.getSuit() == 1){
+      type = 'd';
+    }else if(elem.getSuit() == 2){
+      type = 's';
+    }else{
+      type = 'h';
+    }
+    cout << type << " " << elem.getValue() << endl;
+    
   }
- // Iterates through Bob's set and outputs his remaining cards
-  cout << "Bob's cards" << endl;
-  for (auto it = bob.rbegin(); it != bob.rend(); ++it){
-    cout << *it << endl;
+
+  cout << "\nBob's cards: " << endl;
+  for(auto elem : bob){
+    if(elem.getSuit() == 0){
+      type = 'c';
+    }else if(elem.getSuit() == 1){
+      type = 'd';
+    }else if(elem.getSuit() == 2){
+      type = 's';
+    }else{
+      type = 'h';
+    }
+    cout << type << " " << elem.getValue() << endl;
   }
   return 0;
 }
